12fca0d968ba5451fdd58b7450cf648b7c1aa2fe
FlyingFathead <flyingfathead@protonmail.com>
2026-02-17 03:54:02 +0200

    v0.3.3

--- rust_watchdog.py
+++ rust_watchdog.py
@@ -33,7 +33,7 @@ except Exception:
     ZoneInfo = None  # type: ignore
     ZoneInfoNotFoundError = Exception  # type: ignore
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 SMOOTHRESTARTER_URL = "https://umod.org/plugins/smooth-restarter"
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
